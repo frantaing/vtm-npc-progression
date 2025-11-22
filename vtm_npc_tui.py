9551cb41a6259f1b3a2d9ac2ac1bd093bd503f2a
frantan <francinetaing13@gmail.com>
2025-11-23 00:18:21 +0700

    style: create theme file to hold all colors/symbols and initialize in vtm_npc_tui.py
    Other files haven't been updated yet!

--- vtm_npc_tui.py
+++ vtm_npc_tui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-vtm_tui.py
+vtm_npc_Tui.py
 
 This module contains the UI for the tool. It handles all rendering and
 user input, and uses on vtm_npc_logic.py for character management.
@@ -16,6 +16,7 @@ from tui.greeting_view import GreetingView
 from tui.setup_view import SetupView
 from tui.main_view import MainView
 from tui.final_view import FinalView
+from tui import theme # Theme import here!
 
 # --- [TUI ORCHESTRATOR] ---
 class TUIApp:
@@ -25,38 +26,35 @@ class TUIApp:
         self.stdscr = stdscr
         self.character = None
         
-        # Initialize colors
-        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-        curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_BLACK)
-        curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-        
+        # Hide cursor and enable keypad
         curses.curs_set(0)
         self.stdscr.keypad(True)
+        
+        # Initialize the Theme
+        theme.init_colors()
 
     def run(self):
         """Main application orchestrator."""
         is_free_mode = False
         try:
-            # Phase 0: Greeting
+            # 0. Greeting
             greeting_view = GreetingView(self.stdscr)
             is_free_mode = greeting_view.run()
 
-            # Phase 1: Setup
+            # 1. Setup
             setup_view = SetupView(self.stdscr)
             self.character = setup_view.run(is_free_mode=is_free_mode)
             if not self.character:
                 return 
 
-            # Phase 2: Main Interaction
+            # 2. Main Interaction
             main_view = MainView(self.stdscr, self.character)
             main_view.run()
 
         except QuitApplication:
             pass
         
-        # Phase 3: Final Display
+        # 3. Final Display
         if self.character:
             final_view = FinalView(self.stdscr, self.character)
             final_view.show()
